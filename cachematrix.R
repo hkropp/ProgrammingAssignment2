b90b16efe674be681c20f9bca76f82680299db66
hkropp <hkropp@hortonworks.com>
2015-05-31 22:39:42 +0200

    init function

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# This function creates a class-like structure to store a matrix with a cached inverse product. Similar to a matrix
+# object.
+cacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    
+    set <- function(y) {
+        if(nrow(y) != ncol(y)) {
+            message("Matricies don't have the sme row and cols..")
+        }
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function() x
+    setinverse <- function(solve) inverse <<- solve
+    getinverse <- function() inverse
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
 ## Write a short comment describing this function
-
+# This function demonstrate the use of a matrix object cache. It will return a newly calculated invers of the matrix or
+# will use it's cached value, if it exists.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse) && !is.na(inverse)) {
+        message("Cache hit.")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
